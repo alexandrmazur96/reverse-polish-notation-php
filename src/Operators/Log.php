0d1856278b05466fa4326cc2c443bf688780c90c
Olexandr Mazur <alexandrmazur96@gmail.com>
2025-12-17 14:58:09 +0200

    Added Log operator

---
+++ src/Operators/Log.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rpn\Operators;
+
+use InvalidArgumentException;
+use Rpn\Operands\Number;
+use Rpn\Operands\OperandInterface;
+
+use function log;
+
+readonly class Log implements OperatorInterface
+{
+
+    public function apply(OperandInterface $left, OperandInterface $right): OperandInterface
+    {
+        $value = $left->value();
+
+        if ($value <= 0) {
+            throw new InvalidArgumentException("Logarithm undefined for non-positive numbers: $value");
+        }
+
+        return new Number(log($value));
+    }
+}
