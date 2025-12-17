d626f91a16613f2603911cf46bcb6be92553ecbc
Olexandr Mazur <alexandrmazur96@gmail.com>
2025-12-17 14:58:04 +0200

    Added FourthRoot operator

---
+++ src/Operators/FourthRoot.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rpn\Operators;
+
+use Rpn\Operands\Number;
+use Rpn\Operands\OperandInterface;
+
+readonly class FourthRoot implements OperatorInterface
+{
+    public function apply(OperandInterface $left, OperandInterface $right): OperandInterface
+    {
+        return new Number($right->value() ** (1 / 4));
+    }
+}
