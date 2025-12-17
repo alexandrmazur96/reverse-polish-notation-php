078e72544de624689212bfe86860088c14b5a7b7
Olexandr Mazur <alexandrmazur96@gmail.com>
2025-12-17 14:59:19 +0200

    Added Sqrt operator

---
+++ src/Operators/Sqrt.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rpn\Operators;
+
+use InvalidArgumentException;
+use Rpn\Operands\Number;
+use Rpn\Operands\OperandInterface;
+
+use function sqrt;
+
+readonly class Sqrt implements OperatorInterface
+{
+    public function apply(OperandInterface $left, OperandInterface $right): OperandInterface
+    {
+        $value = $left->value();
+
+        if ($value < 0) {
+            throw new InvalidArgumentException("Cannot calculate square root of a negative number: $value");
+        }
+
+        return new Number(sqrt($value));
+    }
+}
