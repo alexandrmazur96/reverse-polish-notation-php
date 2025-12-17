bb838998862b6d74f3825a64aa35721b4aa9ca59
Olexandr Mazur <alexandrmazur96@gmail.com>
2025-12-17 15:22:55 +0200

    Refactor evaluate method to handle unary and binary operators correctly

--- src/Expression.php
+++ src/Expression.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 
 namespace Rpn;
 
+use Rpn\Operands\Number;
 use Rpn\Operands\OperandInterface;
+use Rpn\Operators\CubeRoot;
+use Rpn\Operators\Exp;
+use Rpn\Operators\Factorial;
+use Rpn\Operators\FourthRoot;
+use Rpn\Operators\Log;
 use Rpn\Operators\OperatorInterface;
+use Rpn\Operators\Sqrt;
 use SplStack;
 
 readonly class Expression
@@ -21,17 +28,39 @@ readonly class Expression
     public function evaluate(): float
     {
         $operandsStack = new SplStack();
+
         foreach ($this->parts as $part) {
             if ($part instanceof OperandInterface) {
                 $operandsStack->push($part);
                 continue;
             }
 
-            $rightOperand = $operandsStack->pop();
-            $leftOperand = $operandsStack->pop();
-            $operandsStack->push($part->apply($leftOperand, $rightOperand));
+            // For Binary ops, this is the RIGHT side. For Unary, it's the ONLY side.
+            $operandA = $operandsStack->pop();
+
+            if ($this->isUnary($part)) {
+                // We pass a dummy '0' as the second argument to satisfy the Interface.
+                // Unary implementations (Sqrt, Log) use the $left argument, so we pass $operandA first.
+                $result = $part->apply($operandA, new Number(0));
+            } else {
+                $operandB = $operandsStack->pop();
+
+                $result = $part->apply($operandB, $operandA);
+            }
+
+            $operandsStack->push($result);
         }
 
         return $operandsStack->pop()->value();
+    }
+
+    private function isUnary(OperatorInterface $operator): bool
+    {
+        return $operator instanceof Sqrt
+               || $operator instanceof Factorial
+               || $operator instanceof Log
+               || $operator instanceof Exp
+               || $operator instanceof CubeRoot
+               || $operator instanceof FourthRoot;
     }
 }
