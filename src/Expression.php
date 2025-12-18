f6d9bdb6e66612b186edbb85786bd9c691cd6cb5
Olexandr Mazur <alexandrmazur96@gmail.com>
2025-12-18 12:01:37 +0200

    Refactor Expression class to use ExpressionPartsStream for evaluation and simplify unary operator handling

--- src/Expression.php
+++ src/Expression.php
@@ -4,47 +4,32 @@ declare(strict_types=1);
 
 namespace Rpn;
 
+use Rpn\Enum\OperatorType;
 use Rpn\Exceptions\InvalidExpressionException;
-use Rpn\Operands\Number;
+use Rpn\Exceptions\InvalidOperatorArgumentException;
 use Rpn\Operands\OperandInterface;
-use Rpn\Operators\CubeRoot;
-use Rpn\Operators\Exp;
-use Rpn\Operators\Factorial;
-use Rpn\Operators\FourthRoot;
-use Rpn\Operators\Log;
 use Rpn\Operators\OperatorInterface;
-use Rpn\Operators\Sqrt;
+use Rpn\Stream\ExpressionPartsStream;
 use SplStack;
 
 readonly class Expression
 {
-    /** @var iterable<OperandInterface|OperatorInterface> */
-    private iterable $parts;
-
-    public function __construct(OperandInterface|OperatorInterface ...$parts)
-    {
-        $this->parts = $parts;
-    }
-
-    /** @throws InvalidExpressionException */
-    public function evaluate(): float
+    /** @throws InvalidExpressionException|InvalidOperatorArgumentException */
+    public function evaluate(ExpressionPartsStream $stream): float
     {
         /** @var SplStack<OperandInterface> $operandsStack */
         $operandsStack = new SplStack();
 
-        foreach ($this->parts as $part) {
+        foreach ($stream as $part) {
             if ($part instanceof OperandInterface) {
                 $operandsStack->push($part);
                 continue;
             }
 
-            // For Binary ops, this is the RIGHT side. For Unary, it's the ONLY side.
             $operandA = $this->popOperand($operandsStack);
 
             if ($this->isUnary($part)) {
-                // We pass a dummy '0' as the second argument to satisfy the Interface.
-                // Unary implementations (Sqrt, Log) use the $left argument, so we pass $operandA first.
-                $result = $part->apply($operandA, new Number(0));
+                $result = $part->apply($operandA);
             } else {
                 $operandB = $this->popOperand($operandsStack);
 
@@ -54,7 +39,6 @@ readonly class Expression
             $operandsStack->push($result);
         }
 
-        // The stack must contain exactly one item (the result)
         if ($operandsStack->count() !== 1) {
             throw new InvalidExpressionException('Too many operands remaining.');
         }
@@ -78,11 +62,10 @@ readonly class Expression
 
     private function isUnary(OperatorInterface $operator): bool
     {
-        return $operator instanceof Sqrt
-               || $operator instanceof Factorial
-               || $operator instanceof Log
-               || $operator instanceof Exp
-               || $operator instanceof CubeRoot
-               || $operator instanceof FourthRoot;
+        $type = $operator->getType();
+
+        return $type === OperatorType::UnaryPrefix
+               || $type === OperatorType::UnaryPostfix
+               || $type === OperatorType::Function;
     }
 }
