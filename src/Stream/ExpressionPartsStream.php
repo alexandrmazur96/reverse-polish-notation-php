2a234a1b4927696f24a4b4d88b495a2f7059fbb3
Olexandr Mazur <alexandrmazur96@gmail.com>
2025-12-18 15:37:49 +0200

    Implement serialization for ExpressionPartsStream and add tests for it

--- src/Stream/ExpressionPartsStream.php
+++ src/Stream/ExpressionPartsStream.php
@@ -5,20 +5,28 @@ declare(strict_types=1);
 namespace Rpn\Stream;
 
 use IteratorAggregate;
-use JsonSerializable;
 use Override;
 use Rpn\Operands\OperandInterface;
 use Rpn\Operators\OperatorInterface;
 use Traversable;
 
+use function is_array;
 use function iterator_to_array;
 
 /** @implements IteratorAggregate<int, OperatorInterface|OperandInterface> */
-final readonly class ExpressionPartsStream implements IteratorAggregate, JsonSerializable
+final class ExpressionPartsStream implements IteratorAggregate
 {
+    /** @var array<int, OperatorInterface|OperandInterface> */
+    private array $buffer;
+
     /** @param iterable<int, OperatorInterface|OperandInterface> $source */
     public function __construct(private iterable $source)
     {
+        if (is_array($source)) {
+            $this->buffer = $source;
+        } else {
+            $this->buffer = [];
+        }
     }
 
     /** @param iterable<int, OperatorInterface|OperandInterface> $source */
@@ -31,13 +39,34 @@ final readonly class ExpressionPartsStream implements IteratorAggregate, JsonSer
     #[Override]
     public function getIterator(): Traversable
     {
-        yield from $this->source;
+        if (!empty($this->buffer)) {
+            yield from $this->buffer;
+            return;
+        }
+
+        $buffer = [];
+        foreach ($this->source as $key => $item) {
+            $buffer[$key] = $item;
+            yield $key => $item;
+        }
+
+        $this->buffer = $buffer;
     }
 
-    /** @return array<int, OperatorInterface|OperandInterface> */
-    #[Override]
-    public function jsonSerialize(): array
+    /** @return array{source: array<int, OperatorInterface|OperandInterface>} */
+    public function __serialize(): array
     {
-        return iterator_to_array($this->source);
+        if (empty($this->buffer)) {
+            $this->buffer = iterator_to_array($this->source, true);
+        }
+
+        return ['source' => $this->buffer];
+    }
+
+    /** @param array{source: array<int, OperatorInterface|OperandInterface>} $data */
+    public function __unserialize(array $data): void
+    {
+        $this->source = $data['source'];
+        $this->buffer = $data['source'];
     }
 }
---
+++ tests/StreamSerializationTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rpn\Tests;
+
+use Generator;
+use Rpn\Exceptions\InvalidExpressionException;
+use Rpn\Exceptions\InvalidOperatorArgumentException;
+use Rpn\Expression;
+use Rpn\Operands\Number;
+use Rpn\Operators\Math\Addition;
+use Rpn\Operators\Math\Multiplication;
+use Rpn\Stream\ExpressionPartsStream;
+
+use function serialize;
+use function unserialize;
+
+final class StreamSerializationTest extends TestCase
+{
+    /**
+     * @throws InvalidExpressionException
+     * @throws InvalidOperatorArgumentException
+     */
+    public function testSerialize(): void
+    {
+        $streamSource = static function (): Generator {
+            yield new Number(10);
+            yield new Number(20);
+            yield new Number(30);
+            yield new Multiplication();
+            yield new Addition();
+        };
+
+        $expression = new Expression();
+        $stream = new ExpressionPartsStream($streamSource());
+
+        $this->assertEquals(610, $expression->evaluate($stream)->value());
+
+        $serialized = serialize($stream);
+        $unserializedStream = unserialize($serialized, ['allowed_classes' => true]);
+
+        $this->assertInstanceOf(ExpressionPartsStream::class, $unserializedStream);
+        $this->assertEquals(610, $expression->evaluate($unserializedStream)->value());
+    }
+}
